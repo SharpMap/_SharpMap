81bbd2c22747a264223759d7d5aac46f9b16f06d
John Diss <jaundice@users.noreply.github.com>
2008-04-28 16:58:01 +0000

    1.1 experimental - added default.aspx to SharpMap.Presentation.AspNet.Demo project

---
+++ Branches/1.1experimental/SharpMap.Presentation.AspNet.Demo/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.IO;
+
+namespace SharpMap.Presentation.AspNet.Demo
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+                BindRepeater();
+        }
+
+        private void BindRepeater()
+        {
+            DirectoryInfo di = new DirectoryInfo(Server.MapPath("~/Maps"));
+            FileInfo[] files = di.GetFiles();
+            
+            this.repDemos.DataSource = files;
+            this.repDemos.DataBind();
+        }
+    }
+}
