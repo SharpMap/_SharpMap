12e6525fdb7265e5f255a327f77a16fd3c573a7e
D_Guidi_cp <SND\D_Guidi_cp>
2011-09-20 12:12:07 +0000

    experiments about adding GeoJSON support

---
+++ Branches/0.9-GeoJSON/SharpMap.Demo.Wms/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿namespace SharpMap.Demo.Wms.Controllers
+{
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.RedirectToAction("Openlayers");
+        }
+
+        public ActionResult Openlayers()
+        {
+            return this.View();
+        }
+
+        public ActionResult Polymaps()
+        {
+            return this.View();
+        }
+
+        public ActionResult Geojson()
+        {
+            return this.View();
+        }
+    }
+}
