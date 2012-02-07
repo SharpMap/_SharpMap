3275606e07d6b4e2f8ef9a459a58847fd1fd85bb
Felix Obermaier <fobermaier@users.noreply.github.com>
2012-02-07 09:41:31 +0000

    tagged "$/SharpMap/Trunk" changeset 95837

---
+++ Tags/v0.9/2012-02-02/SharpMap.Demo.Wms/Handlers/StdWmsMapHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace SharpMap.Demo.Wms.Handlers
+{
+    using System;
+    using System.Diagnostics;
+    using System.Web;
+
+    using Web.Wms;
+
+    public class StdWmsMapHandler : AbstractStdMapHandler
+    {
+        public override void ProcessRequest(HttpContext context)
+        {
+            try
+            {
+                HttpRequest request = context.Request;
+                string url = GetFixedUrl(request);
+                Capabilities.WmsServiceDescription description = GetDescription(url);
+                Map map = this.GetMap(request);
+                WmsServer.ParseQueryString(map, description, 10, null);
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex);
+                throw;
+            }
+        }
+    }
+}
