4d3d9a7b30d390b97216a52f16d748d3a917ca51
petlof_cp <SND\petlof_cp>
2013-03-26 19:07:20 +0000

    delete old v1 branch (not valid)

--- Branches/v1.0/SharpMap.Demo.Wms/Wms.ashx
+++
@@ -1,1 +0,0 @@
-﻿<%@ WebHandler Language="C#" Class="SharpMap.Demo.Wms.Handlers.StdWmsMapHandler" %>
--- Branches/v1.0/SharpMap/Rendering/Decoration/IMapDecoration.cs
+++
@@ -1,9 +0,0 @@
-using System.Drawing;
-
-namespace SharpMap.Rendering.Decoration
-{
-    public interface IMapDecoration
-    {
-        void Render(Graphics g, Map map);
-    }
-}
